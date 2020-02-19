28e4f7e50b3b4663ad30c8729514cb245819ec68
simon <simon@pysant.com>
2020-02-19 21:35:56 +0100

    Added boilerplate multiplicative distrubution, sorting terms and combine like terms.

--- haskell/play/simon/app/Main.hs
+++ haskell/play/simon/app/Main.hs
@@ -11,11 +11,11 @@ main :: IO ()
 main = someFunc
 
 data Expr s where -- Syntax of expression DSL.
-    Pow         :: Integer -> Expr s -- Identity Pow 1, i.e x in math (f(x) = x)
-    Const       :: s -> Expr s       -- Constant function f(x) = 1
-    (:+:)       :: Expr s -> Expr s -> Expr s   -- Addition : x+y
-    (:-:)       :: Expr s -> Expr s -> Expr s   -- Subtraction : x-y
+    Pow         :: Integer -> Expr s            -- Identity Pow 1, i.e x in math (f(x) = x)
+    Const       :: s -> Expr s                  -- Constant function f(x) = 1
+    (:+:)       :: Expr s -> Expr s -> Expr s    -- Addition : x+y
     (:*:)       :: Expr s -> Expr s -> Expr s   -- Multiplication : x*y
+    (:-:)       :: Expr s -> Expr s -> Expr s   -- Subtraction : x-y
     (:/:)       :: Expr s -> Expr s -> Expr s   -- Division : x/y
     (:**:)      :: Expr s -> Expr s -> Expr s   -- Exponent : x^y
     LogBase     :: Expr s -> Expr s -> Expr s   -- Log to what base: Log_x y
@@ -28,6 +28,11 @@ data Expr s where -- Syntax of expression DSL.
     Sin         :: Expr s -> Expr s             -- f(x) = sin(x)
     Cos         :: Expr s -> Expr s             -- f(x) = cos(x)
     Tan         :: Expr s -> Expr s             -- f(x) = tan(x)
+    D           :: Expr s -> Expr s             -- f'(x)
+        --deriving (Show)
+
+instance Show ExprTD where
+    show = showExprTD
 
 -- | Maybe multiplication can be a functor over expr.
 instance Functor Expr where
@@ -39,6 +44,7 @@ instance Functor Expr where
     fmap f (e :/: e')   = fmap f e :/: fmap f e'
     fmap f e@(_ :**: _) = undefined -- Have nothing here.
 
+-- | Naive printing of time domain expression.
 showExprTD :: ExprTD -> String
 showExprTD (Pow 1)                        = "x"
 showExprTD (Pow n)                        = "x^" ++ show n
@@ -46,27 +52,27 @@ showExprTD (Const c)                      = show c
 -- Addtion
 showExprTD ((Pow 1)   :+: (Pow 1))           = "x+x"
 showExprTD ((Const c) :+: (Const c'))     = show c ++ "+" ++ show c'
-showExprTD ((Pow 1)      :+: (Const c))      = "x + " ++ show c
+showExprTD ((Pow 1)   :+: (Const c))      = "x + " ++ show c
 showExprTD ((Const c) :+: (Pow 1))           = show c ++ " + x"
 showExprTD ((Const c) :+: e)              = show c ++ " + (" ++ show e ++ ")"
-showExprTD (Pow 1        :+: e)              = "x + (" ++ show e ++ ")"
+showExprTD (Pow 1     :+: e)              = "x + (" ++ show e ++ ")"
 showExprTD (e         :+: (Const c))      = "(" ++ show e ++ ") + " ++ show c
 showExprTD (e         :+: Pow 1)             = "(" ++ show e ++ ") + x"
 showExprTD (e         :+: e')             = "(" ++ show e ++ ") + (" ++ show e' ++ ")"
 -- Subtraction
-showExprTD ((Pow 1)      :-: (Pow 1))           = "x - x"
+showExprTD ((Pow 1)   :-: (Pow 1))           = "x - x"
 showExprTD ((Const c) :-: (Const c'))     = show c ++ "-" ++ show c'
-showExprTD ((Pow 1)      :-: (Const c))      = "x - " ++ show c
+showExprTD ((Pow 1)   :-: (Const c))      = "x - " ++ show c
 showExprTD ((Const c) :-: (Pow 1))           = show c ++ "+ -"
 showExprTD ((Const c) :-: e)              = show c ++ " - (" ++ show e ++ ")"
-showExprTD (Pow 1        :-: e)              = "x - (" ++ show e ++ ")"
+showExprTD (Pow 1     :-: e)              = "x - (" ++ show e ++ ")"
 showExprTD (e         :-: (Const c))      = "(" ++ show e ++ ") - " ++ show c
 showExprTD (e         :-: Pow 1)             = "(" ++ show e ++ ") - x"
 showExprTD (e         :-: e')             = "(" ++ show e ++ ") - (" ++ show e' ++ ")"
 -- Multiplication
-showExprTD ((Pow 1)      :*: (Pow 1))           = "x*x"
+showExprTD ((Pow 1)   :*: (Pow 1))           = "x*x"
 showExprTD ((Const c) :*: (Const c'))     = show c ++ "*" ++ show c'
-showExprTD ((Pow 1)      :*: (Const c))   = show c ++ "*x"
+showExprTD ((Pow 1)   :*: (Const c))   = show c ++ "*x"
 showExprTD ((Const c) :*: (Pow 1))        = show c ++ "*x"
 showExprTD ((Const c) :*: (Pow n))        = undefined
 showExprTD ((Const c) :*: e)              = show c ++ "*(" ++ show e ++ ")"
@@ -117,12 +123,8 @@ showExprTD (Cos e)                        = "cos(" ++ show e ++ ")"
 showExprTD (Tan e)                        = "tan(" ++ show e ++ ")"
 showExprTD (Signum e)                     = "sign(" ++ show e ++ ")"
 
--- | Naive printing of time domain expression.
-instance Show ExprTD where
-    show = showExprTD
-
 idE :: ExprTD
-idE = (Const 1) :*: (Pow 1)
+idE = Pow 1
 
 type Complex = Integer
 
@@ -176,7 +178,45 @@ transform = undefined
 toPartial :: ExprFD -> Maybe ExprFD
 toPartial = undefined
 
-simplifyBrackets = undefined
+-- | Simplify strategy.
+-- Multiply out brackets.
+-- Apply cool exponant rules.
+-- Combine like terms.
+-- Combine contant term.
+
+-- | Pushes multiplication down. TODO fix.
+distribute (Pow n)                             = Pow n
+distribute (Const s)                           = Const s
+distribute (Const s :*: Const s')              = Const (s*s')
+distribute ((Const s) :*: (Const s' :*: e))    = distribute (Const (s*s') :*: e)
+distribute ((Const s) :*: (e :*: Const s'))    = distribute (Const (s*s') :*: e)
+distribute ((Const s) :*: (e :+: e'))          = distribute (Const s :*: e) :+: distribute (Const s :*: e')
+distribute (e :+: e')                          = (distribute e) :+: (distribute e')
+distribute e = e
+
+combine (Pow s)                                         = Pow s
+combine (Const s)                                       = Const s
+combine (Const s :+: Const s')                          = Const (s+s')
+combine (Const s :+: (Const s' :+: e) )                 = combine $ Const (s+s') :+: e
+combine (Const s :+: (e :+: Const s') )                 = combine $ Const (s+s') :+: e
+combine ((Const s' :+: e) :+: Const s )                 = combine $ Const (s+s') :+: e
+combine ((e :+: Const s') :+: Const s )                 = combine $ Const (s+s') :+: e
+combine e@((Const s :*: Pow n) :+: (Const s' :*: Pow n'))   | n == n'   = Const(s+s') :*: Pow n
+                                                            | otherwise = e
+combine (e :+: e')                                      = combine e :+: combine e'
+combine e                                               = e
+
+shift (Pow s)                                       = Pow s
+shift (Const s)                                     = Const s
+shift (Const s :+: e)                               = Const s   :+: shift e
+shift (e :+: Const s)                               = Const s   :+: shift e
+shift ((Const s :+: e) :+: e'@(Const _ :*: Pow _))  = Const s   :+: shift (e :+: e')
+shift ((Const s :+: e) :+: e')                      = Const s   :+: e' :+: e
+shift (Pow n :+: Pow n')    | n > n'                = Pow n'    :+: Pow n
+                            | otherwise             = Pow n     :+: Pow n'
+shift e                                             = e
+
+
 
 -- | Simplifies an expression.
 --simplifyExpr :: Fractional (Expr a) => Expr a -> Expr a
@@ -189,7 +229,7 @@ simplifyExpr ((Const c) :+: (Const c')) = Const (c + c')
 simplifyExpr (e :+: e')                 = simplifyExpr e + simplifyExpr e'
 simplifyExpr ((Const c) :-: (Const c')) = Const (c - c')
 simplifyExpr (e :-: e')                 = simplifyExpr e - simplifyExpr e'
-simplifyExpr ((Pow n) :*: (Pow n'))      = Pow (n + n')
+simplifyExpr ((Pow n) :*: (Pow n'))     = Pow (n + n')
 simplifyExpr ((Const 0) :*: _)          = 0
 simplifyExpr (_ :*: (Const 0))          = 0
 simplifyExpr e@((Const c) :*: (Pow n))  = e
@@ -253,13 +293,13 @@ evalExpr (Tan e)        = tan . evalExpr e
 evalExpr (Sqrt e)       = sqrt . evalExpr e
 evalExpr (Log e)        = log . evalExpr e
 
--- | f(t) = 2*t + (t +2)*3
+-- | f(t) = 2(2 + 2(2 + x)) + 2 + 2x
 example :: ExprTD
-example = 2 * idE + (2 + idE * idE)*3
+example = 2*(2 + 2*(2 + idE)) + 2
 
--- | f(t) = 2*pi
+-- | f(t) = 2x + 3(2 + x^2)
 example1 :: ExprTD
-example1 = 2*pi / 4
+example1 = 2 * idE + (2 + idE * idE)*3
 
 test = evalExpr example $ 2 -- == 16
 
